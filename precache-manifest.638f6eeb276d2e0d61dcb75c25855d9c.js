09d1737b12d4f82e96dae6ffc4a02f664a0a88f5
Tiến Đạt <47631594+ntdat104@users.noreply.github.com>
2020-10-05 09:37:14 +0700

    Updates

---
+++ precache-manifest.638f6eeb276d2e0d61dcb75c25855d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70af397feb7943d70135cfdc00b395eb",
+    "url": "/admin-app/index.html"
+  },
+  {
+    "revision": "5c94bbbce1b1ac74aaa4",
+    "url": "/admin-app/static/css/main.939017b6.chunk.css"
+  },
+  {
+    "revision": "eae07b9f609e7b99428b",
+    "url": "/admin-app/static/js/2.0487bd35.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/admin-app/static/js/2.0487bd35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c94bbbce1b1ac74aaa4",
+    "url": "/admin-app/static/js/main.8b6613e5.chunk.js"
+  },
+  {
+    "revision": "ce55d382547f032acc81",
+    "url": "/admin-app/static/js/runtime-main.a4d4fdf6.js"
+  }
+]);
